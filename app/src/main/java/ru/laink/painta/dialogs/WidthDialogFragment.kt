7a2d0304b93aa47b6f636b626f67c49a165bdd45
Ivan Potapov <potapov_ivan677@outlook.com>
2020-01-09 12:56:50 +0300

    Changing color by palette

--- app/src/main/java/ru/laink/painta/dialogs/WidthDialogFragment.kt
+++ app/src/main/java/ru/laink/painta/dialogs/WidthDialogFragment.kt
@@ -14,19 +14,17 @@ class WidthDialogFragment : MainDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val paintView = getPainView()!!.painView
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle(R.string.width_dialog_message)
         val view = activity?.layoutInflater?.inflate(R.layout.line_width, null)
-        builder.setView(view)
-
         val seekBar = view?.findViewById<SeekBar>(R.id.seekbar_width)
         seekBar!!.progress = paintView.getLineWidth()
 
-        builder.setPositiveButton(R.string.ok) { _, _ ->
-            paintView.setLineWidth(seekBar.progress.toFloat())
-        }
-
-        builder.setNegativeButton(R.string.cancel, null)
+        val builder = AlertDialog.Builder(context)
+            .setTitle(R.string.width_dialog_message)
+            .setView(view)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                paintView.setLineWidth(seekBar.progress.toFloat())
+            }
+            .setNegativeButton(R.string.cancel, null)
 
         return builder.create()
     }
